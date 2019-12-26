7da87aa9c462b13bc0953d3aa19ee7d64cb3f0f6
Nhân Nguyễn <nhan28061997@gmail.com>
2019-12-26 08:30:02 +0700

    End of section 4

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { NEXT } from '@angular/core/src/render3/interfaces/view';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      next => {
+        console.log('Logged in successfully!');
+      },
+      error => {
+        console.log('Failed to login!');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+}
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -29,7 +29,7 @@ namespace DatingApp.API.Data
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordHash))
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 for(int i = 0; i < computeHash.Length; i++)
