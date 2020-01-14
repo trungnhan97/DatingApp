ace352bae75408eac77fb7c52576e69d113a7bf3
Nhân Nguyễn <nhan28061997@gmail.com>
2020-01-14 14:49:02 +0700

    End of section 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MemberEditComponent } from './member/member-edit/member-edit.component'
 import { MemberEditlResolver } from './_resolver/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './member/photo-editor/photo-editor.component';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -48,7 +49,8 @@ export function tokenGetter() {
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -57,6 +57,7 @@ namespace DatingApp.API
                     };
                 });
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
